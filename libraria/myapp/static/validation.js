1fb8cb9ea848fccfc5102a4d3e750446ad91b9b8
josemanuelponce <josee.manuel.9700@gmail.com>
2023-03-03 01:23:17 +0100

    index actualizado

--- libraria/myapp/static/validation.js
+++ libraria/myapp/static/validation.js
@@ -1,36 +1,3 @@
-var form = document.getElementById("createForm");
-
-const tittle = document.getElementById("tittle_input");
-const genres = document.getElementById("genres_input");
-const authors = document.getElementById("authors_input");
-
-var clearButton = document.getElementById("clearButton");
-var hideButton = document.getElementById("hideButton");
-var showButton = document.getElementById("showButton");
-
-function onSubmitClicked(event) {
-    if(tittle.value.trim().length === 0) {
-        tittle.classList.add("error");
-        alert("Tittle required");
-        return false;
-    } else {
-        tittle.classList.remove("error");
-    }
-
-    if(genres.value.trim().length === 0) {
-        genres.classList.add("error");
-        alert("genres required");
-        return false;
-    } 
-
-    if(authors.value.trim().length === 0) {
-        authors.classList.add("error");
-        alert("authors required");
-        return false;
-    } 
-
-    return false;
-}
 
 function clear(){
     tittle.value = "";
@@ -51,3 +18,34 @@ function show(){
  clearButton.onclick = clear;
  hideButton.onclick = hide;
  showButton.onclick = show;
+
+ //--------------------------------
+
+
+ const form = document.querySelector('#createForm');
+		form.addEventListener('submit', (event) => {
+			// Detiene el envío del formulario
+			event.preventDefault();
+			// Verifica que todos los campos requeridos estén completos
+			if (form.checkValidity() === false) {
+				event.stopPropagation();
+			} else {
+				// Enviar formulario
+				form.submit();
+			}
+			form.classList.add('was-validated');
+		})
+
+		// Agrega la validación de género
+		// const genreInput = document.querySelector('#genre');
+		// genreInput.addEventListener('change', (event) => {
+		// 	const selectedGenre = event.target.value;
+		// 	if (selectedGenre === '') {
+		// 		genreInput.setCustomValidity('Por favor, selecciona un género');
+		// 	} else {
+		// 		genreInput.setCustomValidity('');
+		// 	}
+		// })
+
+
+
